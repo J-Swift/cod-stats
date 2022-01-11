a7245b9c14da90f6735992659a1a555df4f57538
Jimmy Reichley <jimmyqpublik@gmail.com>
2022-01-11 17:12:22 -0500

    Formatting

--- parser/parse_matches.sh
+++ parser/parse_matches.sh
@@ -199,7 +199,11 @@ CREATE VIEW vw_core_players AS
 
 DROP VIEW IF EXISTS vw_unknown_modes_wz;
 CREATE VIEW vw_unknown_modes_wz AS
-  SELECT json_extract(stats, '$.mode') mode, strftime('%Y-%m-%dT%H:%M:%SZ', min(json_extract(stats, '$.utcEndSeconds')), 'unixepoch') firstSeen, strftime('%Y-%m-%dT%H:%M:%SZ', max(json_extract(stats, '$.utcEndSeconds')), 'unixepoch') lastSeen, count(1) totalGames
+  SELECT
+    json_extract(stats, '$.mode') mode,
+    strftime('%Y-%m-%dT%H:%M:%SZ', min(json_extract(stats, '$.utcEndSeconds')), 'unixepoch') firstSeen,
+    strftime('%Y-%m-%dT%H:%M:%SZ', max(json_extract(stats, '$.utcEndSeconds')), 'unixepoch') lastSeen,
+    count(1) totalGames
   FROM raw_games
   WHERE json_extract(stats, '$.gameType')='wz' AND mode NOT IN (SELECT id FROM vw_game_modes WHERE mode='wz')
   GROUP BY mode
